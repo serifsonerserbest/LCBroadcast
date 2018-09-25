28a6fcf42b9b4a4733679af5a447aae3d07a5fad
Paul <paul@gmail.com>
2018-09-26 01:40:44 +0300

    logger added

---
+++ src/com/epfl/da/Logger.java
@@ -0,0 +1,34 @@
+package com.epfl.da;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Logger {
+    private List<String> Log;
+    public final String outputFileName;
+
+    public Logger(int processId) {
+        Log = new ArrayList<String>();
+        outputFileName = "da_proc_" + processId + ".out";
+    }
+
+    public void WriteToLog (final String message) {
+        if(message != null && !message.trim().isEmpty())
+        {
+            Log.add(message);
+        }
+    }
+
+    public void WriteLogToFile () {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName))){
+            for (String message : Log) {
+                writer.write(message);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
