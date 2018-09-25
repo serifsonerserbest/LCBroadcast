63cee761d01da07a2e5e590c72250a5fb43e45db
mammadhajili <mammadhajili@gmail.com>
2018-09-25 10:29:34 +0200

    simple change

--- src/com/epfl/da/Main.java
+++ src/com/epfl/da/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello"+"\n"+"world");
         System.out.println("hello"+"\n"+"world");
+        System.out.println(2);
     }
 }
