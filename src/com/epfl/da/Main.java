fc8f3c8bf54455e5206bdcb0e1885c1336956594
Paul <paul@gmail.com>
2018-09-25 12:56:59 +0300

    signals handler added

--- src/com/epfl/da/Main.java
+++ src/com/epfl/da/Main.java
@@ -1,11 +1,30 @@
 package com.epfl.da;
 
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.Process;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("hello"+"\n"+"world");
-        System.out.println("hello"+"\n"+"world");
-        System.out.println(2);
-        System.out.println("Best team ever");
+    public static void main(String[] args) throws InterruptedException {
+        SignalHandler signalHandler = new SignalHandler() {
+            @Override
+            public void handle(Signal sig) {
+                System.out.println("Abort");
+            }
+        };
+
+
+        DiagnosticSignalHandler.install("TERM", signalHandler);
+        DiagnosticSignalHandler.install("INT", signalHandler);
+        DiagnosticSignalHandler.install("ABRT", signalHandler);
+
+        System.out.println("Started");
+        int i = 0;
+        while (true) {
+        }
     }
 }
