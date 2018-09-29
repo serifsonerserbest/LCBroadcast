38f3b1d61b40fdcd75eb50969bca371e570c6fe7
mammadhajili <mammadhajili@gmail.com>
2018-09-29 17:00:52 +0200

    adding sendAck

--- src/com/epfl/da/Receiver.java
+++ src/com/epfl/da/Receiver.java
@@ -67,13 +67,24 @@ public class Receiver {
             int content = messageArray[1];
             Message message = new Message(messageId, addressReceived);
             if(isReceived.contains(message)) {
+                ackType = 0;
                 System.out.println("Message #" + messageId + ": " + content + " duplicate");
             }
             else {
+                ackType = 1;
                 System.out.println("Message #" + messageId + ": " + content + " was successfully received");
                 isReceived.add(message);
             }
+            sendAck(socketIn, portReceived, addressReceived, ackType, messageId);
         }
 
+    }
+    public void sendAck(DatagramSocket socket, int port, InetAddress address, int ackType, int messageId) throws IOException {
+        int[] data = {ackType, messageId};
+        ByteBuffer byteBuffer = ByteBuffer.allocate(data.length * 4);
+        IntBuffer intBuffer = byteBuffer.asIntBuffer();
+        intBuffer.put(data);
+        byte[] sentData = byteBuffer.array();
+        socket.send(new DatagramPacket(sentData, sentData.length, address, port));
     }
 }
