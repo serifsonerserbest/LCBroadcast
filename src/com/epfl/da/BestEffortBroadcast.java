594965e4149ad14df5af6b5c54b209cedd88b8dd
serifsonerserbest <43368426+serifsonerserbest@users.noreply.github.com>
2018-10-03 14:06:42 +0200

    Best Effort Broadcast Implementation - Soner

---
+++ src/com/epfl/da/BestEffortBroadcast.java
@@ -0,0 +1,28 @@
+package com.epfl.da;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+public class BestEffortBroadcast {
+
+    private PerfectLink perfectlink;
+
+    public BestEffortBroadcast(){
+        perfectlink = new PerfectLink();
+    }
+
+    public void Broadcast(int message, InetAddress[] addresses, int[] ports)
+    {
+        //TODO: One must also send the mesage to itself in broadcasting !!!
+        //perfectlink.Send(message, OWN_ADDRESS, OWN_PORT);
+
+        for (int i = 0; i < addresses.length; i++) {
+            perfectlink.Send(message, addresses[i], ports[i]);
+        }
+    }
+
+    public void Deliver(int port) throws IOException {
+        perfectlink.Deliver(port);
+    }
+}
+
--- src/com/epfl/da/Main.java
+++ src/com/epfl/da/Main.java
@@ -13,12 +13,12 @@ import java.net.UnknownHostException;
 
 public class Main {
 
-    private static void TestReceive() throws IOException {
+    private static void TestReceivePL() throws IOException {
         PerfectLink server = new PerfectLink();
         server.Deliver(20002);
     }
 
-    private static void TestSend() throws UnknownHostException {
+    private static void TestSendPL() throws UnknownHostException {
         PerfectLink client = new PerfectLink();
         for (int x = 1; x <= 1000; x++){
             client.Send(x, InetAddress.getByName("127.0.0.1"), 20002);
@@ -28,8 +28,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException, IOException {
         Process process = new Process(1);
 
-        //TestReceive();
-        //TestSend();
-
+        //TestReceivePL();
+        //TestSendPL();
     }
 }
